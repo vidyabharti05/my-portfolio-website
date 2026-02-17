f8dd1406ce444ec2841822764691b71772962637
Vidya Bharti <vidyabharti2605@gmail.com>
2026-02-17 20:02:53 +0530

    Update CSS for full responsiveness

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 //https//
 export default defineConfig({
   plugins: [react()],
-  base: '/portfolio-website/',  // must match your repo name
+  base: '/my-portfolio-website/',  // must match your repo name
 });
